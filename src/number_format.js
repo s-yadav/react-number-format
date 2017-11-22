634d3409113c6fe3558ec7e81d9dc84ad673d5bc
Vytautas Perliba <vytautas.per@gmail.com>
2017-11-22 15:45:20 +0200

    Add optional autoFocus prop (fixes #122)

--- src/number_format.js
+++ src/number_format.js
@@ -45,7 +45,8 @@ const propTypes = {
   onBlur: PropTypes.func,
   type: PropTypes.oneOf(['text', 'tel']),
   isAllowed: PropTypes.func,
-  renderText: PropTypes.func
+  renderText: PropTypes.func,
+  autoFocus: PropTypes.bool
 };
 
 const defaultProps = {
@@ -63,7 +64,8 @@ const defaultProps = {
   onMouseUp: noop,
   onFocus: noop,
   onBlur: noop,
-  isAllowed: returnTrue
+  isAllowed: returnTrue,
+  autoFocus: false
 };
 
 class NumberFormat extends React.Component {
@@ -747,7 +749,7 @@ class NumberFormat extends React.Component {
   }
 
   render() {
-    const {type, displayType, customInput, renderText} = this.props;
+    const {type, displayType, customInput, renderText, autoFocus} = this.props;
     const {value} = this.state;
 
     const otherProps = omit(this.props, propTypes);
@@ -755,6 +757,7 @@ class NumberFormat extends React.Component {
     const inputProps = Object.assign({}, otherProps, {
       type,
       value,
+      autoFocus,
       onChange: this.onChange,
       onKeyDown: this.onKeyDown,
       onMouseUp: this.onMouseUp,
