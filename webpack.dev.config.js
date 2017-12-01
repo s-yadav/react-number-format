a0143ca10f5e8905eee428226c58570c338111c8
Mohit Gupta <mohitgupta8888@gmail.com>
2017-12-01 19:26:51 +0530

    Reverted devTool setting in webpack.dev.config.js

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -4,7 +4,7 @@ module.exports = {
       './example/src/index.js'
     ]
   },
-  //devtool: "eval-source-map",
+  devtool: "eval-source-map",
   debug: true,
   output: {
     publicPath: "http://localhost:9000/",
