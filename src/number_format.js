24ca625e540f3809a794898977a6e38bfe34bfc6
David Hartman <David.Hartman@geico.com>
2019-08-06 12:41:12 -0400

    Remove destructuring of unused variable

--- src/number_format.js
+++ src/number_format.js
@@ -457,7 +457,7 @@ class NumberFormat extends React.Component {
    * @return {string} formatted Value
    */
   formatAsNumber(numStr: string) {
-    const {decimalScale, fixedDecimalScale, prefix, suffix, allowNegative, thousandsGroupStyle, value} = this.props;
+    const {decimalScale, fixedDecimalScale, prefix, suffix, allowNegative, thousandsGroupStyle} = this.props;
     const {thousandSeparator, decimalSeparator} = this.getSeparators();
 
     const hasDecimalSeparator = numStr.indexOf('.') !== -1 || (decimalScale && fixedDecimalScale);
