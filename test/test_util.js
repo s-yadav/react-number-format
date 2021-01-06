c826381122b15ef5e3e9d1a9aa5cf725e544b723
Naman Malhotra <naman03malhotra@gmail.com>
2021-01-07 00:12:10 +0530

    [Temp] temp commit to test CD on tag push

--- test/test_util.js
+++ test/test_util.js
@@ -1,19 +1,19 @@
-import Enzyme, {shallow, mount} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, { shallow, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-const noop = function(){};
+const noop = function () {};
 
 export const persist = jasmine.createSpy();
 
-//keep input element singleton
-const target = document.createElement('input');
+//keep input element singleton asd
+const target = document.createElement("input");
 
 export function getCustomEvent(value, selectionStart, selectionEnd) {
-  let event =  new Event('custom');
-  const el = document.createElement('input');
-  event = {...event, target: el, persist};
+  let event = new Event("custom");
+  const el = document.createElement("input");
+  event = { ...event, target: el, persist };
   event.target = el;
   el.value = value;
   el.selectionStart = selectionStart;
@@ -22,136 +22,169 @@ export function getCustomEvent(value, selectionStart, selectionEnd) {
 }
 
 function getEvent(eventProps, targetProps) {
-  let event =  new Event('custom');
+  let event = new Event("custom");
 
   Object.keys(targetProps).forEach((key) => {
     target[key] = targetProps[key];
   });
 
-  event = {...event, ...eventProps, target};
+  event = { ...event, ...eventProps, target };
 
   return event;
 }
 
 export function setCaretPosition(event, caretPos) {
-  const {target} = event;
+  const { target } = event;
   target.focus();
   target.setSelectionRange(caretPos, caretPos);
 }
 
-
-export function simulateKeyInput(input, key, selectionStart, selectionEnd, setSelectionRange) {
+export function simulateKeyInput(
+  input,
+  key,
+  selectionStart,
+  selectionEnd,
+  setSelectionRange
+) {
   if (selectionEnd === undefined) {
     selectionEnd = selectionStart;
   }
 
-  const currentValue = input.prop('value');
+  const currentValue = input.prop("value");
   let defaultPrevented = false;
 
-  const keydownEvent = getEvent({
-    preventDefault: function() {
-      defaultPrevented = true;
+  const keydownEvent = getEvent(
+    {
+      preventDefault: function () {
+        defaultPrevented = true;
+      },
+      key,
+      isUnitTestRun: true,
+      persist: persist.bind(null, "keydown"),
     },
-    key,
-    isUnitTestRun: true,
-    persist: persist.bind(null, 'keydown')
-  }, {
-    value: currentValue,
-    selectionStart,
-    selectionEnd,
-    setSelectionRange: setSelectionRange || noop,
-    focus: noop
-  })
+    {
+      value: currentValue,
+      selectionStart,
+      selectionEnd,
+      setSelectionRange: setSelectionRange || noop,
+      focus: noop,
+    }
+  );
 
   //fire key down event
-  input.simulate('keydown', keydownEvent);
+  input.simulate("keydown", keydownEvent);
 
   //fire change event
-  if (!defaultPrevented && key !== 'ArrowLeft' && key !== 'ArrowRight') {
-
+  if (!defaultPrevented && key !== "ArrowLeft" && key !== "ArrowRight") {
     //get changed caret positon
     let newCaretPosition, newValue;
 
-    if (key === 'Backspace') {
-      newCaretPosition = selectionStart !== selectionEnd ? selectionStart : selectionStart - 1;
-      newValue = selectionStart !== selectionEnd ?
-        currentValue.substring(0, selectionStart) + currentValue.substring(selectionEnd, currentValue.length) :
-        currentValue.substring(0, newCaretPosition) + currentValue.substring(selectionStart, currentValue.length);
-    } else if (key === 'Delete') {
+    if (key === "Backspace") {
+      newCaretPosition =
+        selectionStart !== selectionEnd ? selectionStart : selectionStart - 1;
+      newValue =
+        selectionStart !== selectionEnd
+          ? currentValue.substring(0, selectionStart) +
+            currentValue.substring(selectionEnd, currentValue.length)
+          : currentValue.substring(0, newCaretPosition) +
+            currentValue.substring(selectionStart, currentValue.length);
+    } else if (key === "Delete") {
       newCaretPosition = selectionStart;
-      newValue = selectionStart !== selectionEnd ?
-        currentValue.substring(0, selectionStart) + currentValue.substring(selectionEnd, currentValue.length) :
-        currentValue.substring(0, selectionStart) + currentValue.substring(selectionStart + 1, currentValue.length);
+      newValue =
+        selectionStart !== selectionEnd
+          ? currentValue.substring(0, selectionStart) +
+            currentValue.substring(selectionEnd, currentValue.length)
+          : currentValue.substring(0, selectionStart) +
+            currentValue.substring(selectionStart + 1, currentValue.length);
     } else {
       newCaretPosition = selectionStart + key.length;
-      newValue = selectionStart !== selectionEnd ?
-        currentValue.substring(0, selectionStart) + key + currentValue.substring(selectionEnd, currentValue.length) :
-        currentValue.substring(0, selectionStart) + key + currentValue.substring(selectionStart, currentValue.length);
+      newValue =
+        selectionStart !== selectionEnd
+          ? currentValue.substring(0, selectionStart) +
+            key +
+            currentValue.substring(selectionEnd, currentValue.length)
+          : currentValue.substring(0, selectionStart) +
+            key +
+            currentValue.substring(selectionStart, currentValue.length);
     }
 
-    const changeEvent = getEvent({
-      persist: persist.bind(null, 'change'),
-      key
-    }, {
-      value: newValue,
-      selectionStart: newCaretPosition,
-      selectionEnd: newCaretPosition,
-      setSelectionRange: setSelectionRange || noop,
-      focus: noop
-    })
-    input.simulate('change', changeEvent);
+    const changeEvent = getEvent(
+      {
+        persist: persist.bind(null, "change"),
+        key,
+      },
+      {
+        value: newValue,
+        selectionStart: newCaretPosition,
+        selectionEnd: newCaretPosition,
+        setSelectionRange: setSelectionRange || noop,
+        focus: noop,
+      }
+    );
+    input.simulate("change", changeEvent);
   }
 }
 
 export function simulateMousUpEvent(input, selectionStart, setSelectionRange) {
   const selectionEnd = selectionStart;
 
-  const currentValue = input.prop('value');
+  const currentValue = input.prop("value");
 
-  const mouseUpEvent = getEvent({}, {
-    value: currentValue,
-    selectionStart,
-    selectionEnd,
-    setSelectionRange: setSelectionRange || noop,
-    focus: noop
-  });
+  const mouseUpEvent = getEvent(
+    {},
+    {
+      value: currentValue,
+      selectionStart,
+      selectionEnd,
+      setSelectionRange: setSelectionRange || noop,
+      focus: noop,
+    }
+  );
 
-  input.simulate('mouseup', mouseUpEvent);
-
+  input.simulate("mouseup", mouseUpEvent);
 }
 
-export function simulateFocusEvent(input, selectionStart = 0, selectionEnd, setSelectionRange) {
+export function simulateFocusEvent(
+  input,
+  selectionStart = 0,
+  selectionEnd,
+  setSelectionRange
+) {
   if (selectionEnd === undefined) {
     selectionEnd = selectionStart;
   }
 
-  const currentValue = input.prop('value');
+  const currentValue = input.prop("value");
 
-  const focusEvent = getEvent({
-    persist: persist.bind(null, 'focus')
-  }, {
-    value: currentValue,
-    selectionStart,
-    selectionEnd,
-    setSelectionRange: setSelectionRange || noop,
-    focus: noop
-  });
+  const focusEvent = getEvent(
+    {
+      persist: persist.bind(null, "focus"),
+    },
+    {
+      value: currentValue,
+      selectionStart,
+      selectionEnd,
+      setSelectionRange: setSelectionRange || noop,
+      focus: noop,
+    }
+  );
 
-  input.simulate('focus', focusEvent);
-
+  input.simulate("focus", focusEvent);
 }
 
 export function simulateBlurEvent(input) {
-  const currentValue = input.prop('value');
+  const currentValue = input.prop("value");
 
-  const blurEvent = getEvent({
-    persist: persist.bind(null, 'blur')
-  }, {
-    value: currentValue,
-  });
+  const blurEvent = getEvent(
+    {
+      persist: persist.bind(null, "blur"),
+    },
+    {
+      value: currentValue,
+    }
+  );
 
-  input.simulate('blur', blurEvent);
+  input.simulate("blur", blurEvent);
 }
 
-
-export {Enzyme, shallow, mount};
+export { Enzyme, shallow, mount };
