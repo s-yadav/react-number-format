dd7fe3b4787f193f19757df3b28e4454ed7d8d08
Sudhanshu <sudhanshuyadav2@gmail.com>
2022-05-02 22:53:55 +0530

    Add modular imports

--- typings/src/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.SourceType = void 0;
-var SourceType;
-(function (SourceType) {
-    SourceType["event"] = "event";
-    SourceType["props"] = "prop";
-})(SourceType = exports.SourceType || (exports.SourceType = {}));
