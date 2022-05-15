91822af8e3fb030b4703aa758a842038cc17d1e0
Sudhanshu <sudhanshuyadav2@gmail.com>
2022-05-15 12:11:15 +0530

    Fix build

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
 import NumberFormatBase from './number_format_base';
 import NumericFormat from './numeric_format';
 import PatternFormat from './pattern_format';
+import {
+  format as numericFormatter,
+  removeFormatting as removeNumericFormat,
+  getCaretBoundary as getNumericCaretBoundary,
+  useNumericFormat,
+} from './numeric_format';
+
+import {
+  format as patterFormatter,
+  removeFormatting as removePatternFormat,
+  getCaretBoundary as getPatternCaretBoundary,
+  usePatternFormat,
+} from './pattern_format';
+
 import { NumericFormatProps, NumberFormatBaseProps, PatternFormatProps } from './types';
 
 export { NumberFormatBase, NumericFormat, PatternFormat };
 export { NumericFormatProps, NumberFormatBaseProps, PatternFormatProps };
+
+export { numericFormatter, removeNumericFormat, getNumericCaretBoundary, useNumericFormat };
+export { patterFormatter, removePatternFormat, getPatternCaretBoundary, usePatternFormat };
