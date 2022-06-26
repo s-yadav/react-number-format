546908a6fa91934e9da7b5c1a6e9f42e7a335d9c
Nikhil Varma <nikhilvarma892@gmail.com>
2022-06-26 21:25:43 +0530

    fix: Fix intro page

--- documentation/v5/src/components/HomepageFeatures/index.js
+++ documentation/v5/src/components/HomepageFeatures/index.js
@@ -4,44 +4,42 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
+    title: 'Prefix, suffix and thousand separator.',
+    description: <></>,
   },
   {
     title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
+    description: <></>,
   },
   {
     title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
+    description: <></>,
+  },
+  {
+    title: 'Custom format pattern.',
+    description: <></>,
+  },
+
+  {
+    title: 'Masking.',
+    description: <></>,
+  },
+
+  {
+    title: 'Custom formatting handler.',
+    description: <></>,
+  },
+  {
+    title: 'Format number in an input or format as a simple text.',
+    description: <></>,
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description }) {
   return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
+    <div>
+      <div className="text--left">{/* <Svg className={styles.featureSvg} role="img" /> */}</div>
+      <div className="text--left padding-horiz--md">
         <h3>{title}</h3>
         <p>{description}</p>
       </div>
@@ -53,11 +51,10 @@ export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
+        <h1>Features</h1>
+        {FeatureList.map((props, idx) => (
+          <Feature key={idx} {...props} />
+        ))}
       </div>
     </section>
   );
