f0399a33d5f13c9baa16f91cd602baf258e16062
Nikhil Varma <nikhilvarma892@gmail.com>
2022-06-26 21:25:43 +0530

    fix: Fix intro page

--- documentation/v5/docusaurus.config.js
+++ documentation/v5/docusaurus.config.js
@@ -6,9 +6,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'React Number format',
+  tagline: 'React component to format numbers in an input or as a text.',
+  url: 'https://github.com/s-yadav/react-number-format',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
