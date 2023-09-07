5a78dfd0c7444fafd13ac9b6e1a7735120203c0a
Aashu Anshuman <aashu.anshuman@gmail.com>
2023-09-07 17:38:32 +0100

    Pull updates to input_numeric_format.spec

--- test/library/input_numeric_format.spec.jsx
+++ test/library/input_numeric_format.spec.jsx
@@ -771,6 +771,61 @@ describe('Test NumberFormat as input with numeric format options', () => {
     expect(input).toHaveValue('0.00000001');
   });
 
+  it('should handle formatting correctly when valueIsNumericString is set to false and float value is provided, #741', async () => {
+    const { input } = await render(
+      <NumericFormat value={12345} valueIsNumericString={false} thousandSeparator={true} />,
+    );
+
+    expect(input.value).toEqual('12,345');
+  });
+
+  it('should not infinite rerender when valueIsNumericString is not set and decimalScale is provided, and values.value is used inside onValueChange #786', async () => {
+    const ControlledComponent = (props) => {
+      const [value, setValue] = useState('');
+      const [renderCount, setRenderCount] = useState(0);
+
+      return (
+        <>
+          <NumericFormat
+            value={value}
+            onValueChange={(values) => {
+              //return to avoid infinite rerender
+              if (renderCount > 10) return;
+              setValue(values.value);
+              setRenderCount(renderCount + 1);
+            }}
+            {...props}
+          />
+          <span data-testid="renderCount">{renderCount}</span>
+        </>
+      );
+    };
+    const { input, user, view } = await render(
+      <ControlledComponent
+        thousandSeparator={false}
+        decimalSeparator=","
+        decimalScale={2}
+        fixedDecimalScale
+      />,
+    );
+
+    await simulateKeyInput(user, input, '2', 0, 0);
+
+    const renderCount = await view.getByTestId('renderCount');
+    expect(renderCount.innerHTML).toEqual('1');
+    expect(input.value).toEqual('2,00');
+  });
+
+  it('should not delete decimal separator if delete key is pressed before decimal separator when fixedDecimalScale is provided. #789', async () => {
+    const { input, user } = await render(
+      <NumericFormat value={'123.000'} decimalScale={3} fixedDecimalScale />,
+    );
+
+    await simulateKeyInput(user, input, '{delete}', 3, 3);
+
+    expect(input.value).toEqual('123.000');
+  });
+
   describe('should allow typing number if prefix or suffix is just an number #691', () => {
     it('when prefix is number', async () => {
       const { input, user } = await render(<NumericFormat prefix="1" />);
