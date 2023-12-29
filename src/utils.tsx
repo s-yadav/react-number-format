34c0d8ce664e917513ee207eb6b651c4f0e667f0
Triggs <tannerdriggers@gmail.com>
2023-12-29 14:45:43 -0500

    Change to optional focus function call

--- src/utils.tsx
+++ src/utils.tsx
@@ -235,7 +235,7 @@ export function setCaretPosition(el: HTMLInputElement, caretPos: number) {
     }
 
     // fail city, fortunately this never happens (as far as I've tested) :)
-    el.focus();
+    el.focus?.();
     return false;
   }
 }
