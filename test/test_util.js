bf0c31aad01ef7833c81e8ddac410b6c2755ec35
Aashu Anshuman <aashu.anshuman@gmail.com>
2024-03-02 16:44:50 +0000

    Fix import in test_util

--- test/test_util.js
+++ test/test_util.js
@@ -1,6 +1,6 @@
 // @ts-check
 import { expect, afterEach } from 'vitest';
-import matchers from '@testing-library/jest-dom/matchers';
+import * as matchers from '@testing-library/jest-dom/matchers';
 import userEvent from '@testing-library/user-event';
 import { cleanup, render as testRender, fireEvent } from '@testing-library/react';
 
